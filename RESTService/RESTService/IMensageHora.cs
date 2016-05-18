3c858a09be555748f632bce46a10417e7f2feeb1
Orlando Daniel Castillo De La Torre <u201400771@upc.edu.pe>
2016-05-18 01:43:53 -0500

    WSRest

---
+++ RESTService/RESTService/IMensageHora.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace RESTService
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IMensageHora" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IMensageHora
+    {
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json)]
+        int ObtenerHora();
+    }
+}
---
+++ RESTService/RESTService/Mensajes.svc.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace RESTService
+{
+    public class Mensajes : IMensajes
+    {
+        public string ObtenerSaludo()
+        {
+
+            HttpWebRequest req = WebRequest.Create("http://localhost:9550/MensageHora.svc/ObtenerHora") as HttpWebRequest;
+            HttpWebResponse res = req.GetResponse() as HttpWebResponse;
+            StreamReader reader = new StreamReader(res.GetResponseStream());
+            int horasola = int.Parse(reader.ReadToEnd());
+            
+            if (horasola < 12)
+                return "Buenos dias" + horasola.ToString();
+            else if (horasola < 19)
+                return "Buenas tardes" + horasola.ToString(); 
+                else return "Buenas noches" + horasola.ToString();
+        }
+
+    }
+}
