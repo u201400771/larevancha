5c71010c87c0438fd36e16cba9fa08c5ae065775
jdipere@hotmail.com <jdipere@hotmail.com>
2016-05-21 10:16:58 -0500

    juan montes

--- WCFSoap/WCFSoap/IService1.cs
+++ WCFSoap/WCFSoap/IService1.cs
@@ -7,7 +7,7 @@ using System.Text;
 
 namespace WCFSoap
 {
-    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IService1" en el código y en el archivo de configuración a la vez.
+
     [ServiceContract]
     public interface IService1
     {
