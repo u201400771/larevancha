4b6379fc89a19f9fda691dc7bd81ac845e3fdc80
Orlando Daniel Castillo De La Torre <u201400771@upc.edu.pe>
2016-05-29 23:26:29 -0500

    Orlando Castillo - prueba2

---
+++ PAGOSRest/PAGOSRest/Documentos.svc.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using PAGOSRest.Dominio;
+using PAGOSRest.Persistencia;
+using System.ServiceModel.Web;
+using System.Net;
+
+namespace PAGOSRest
+{
+    public class Documentos : IDocumentos
+    {
+        private DocumentoDAO dao = new DocumentoDAO();
+
+        public Documento CrearDocumento(Documento documentoACrear)
+        {
+
+            Documento DocumentoObtenido = dao.Obtener(documentoACrear.numero_documento);
+
+            if (DocumentoObtenido != null)
+            {
+                throw new WebFaultException<string>(
+                    "El cliente con codigo " + documentoACrear.numero_documento + " ya existe", HttpStatusCode.InternalServerError);
+            }
+            return dao.Crear(documentoACrear);
+        }
+
+        public Documento ObtenerDocumento(string codigo)
+        {
+            return dao.Obtener(codigo);
+        }
+
+        public Documento ModificarDocumento(Documento documentoAModificar)
+        {
+            return dao.Modificar(documentoAModificar);
+        }
+
+        public void EliminarDocumento(string numero_documento)
+        {
+            dao.Eliminar(numero_documento);
+        }
+
+        public List<Documento> ListarADocumentos()
+        {
+            return dao.listar();
+        }
+
+    }
+}
