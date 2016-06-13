5928c12edf6d95ed5a15e715f9778faba5db7e3b
jdipere <jdipere@hotmail.com>
2016-06-12 23:01:44 -0500

    juan montes

--- ProyFortaleza_integrado/MensajeriaRest/Persistencia/MensajeDAO.cs
+++ ProyFortaleza_integrado/MensajeriaRest/Persistencia/MensajeDAO.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
-// Mensajeria
+
 namespace MensajeriaRest.Persistencia
 {
     public class MensajeDAO
@@ -12,7 +12,7 @@ namespace MensajeriaRest.Persistencia
         public Mensaje Crear(Mensaje mensajeACrear)
         {
             Mensaje mensajeCreado = null;
-            //            string sql = "INSERT INTO tbl_pedido VALUES (@dni, @nombre)";
+       
             string sql = "UPDATE CHEQUE_CABECERA SET ESTADO = 'FIR' WHERE NUMERO_CHEQUE = @NUMERO_CHEQUE";
             using (SqlConnection con = new SqlConnection(ConexionUtil.CadenaClientes))
             {
@@ -23,34 +23,13 @@ namespace MensajeriaRest.Persistencia
                     com.ExecuteNonQuery();
                 }
             }
-            //            pedidoCreado = Obtener(pedidoACrear.dni);
+          
             return mensajeCreado;
         }
 
         public  List<Mensaje> ListarTodos()
         {
-            /* List<Mensaje> mensajeEncontrados = new List<Mensaje>();
-             Mensaje pedidoEncontrado = null;
-             string sql = "SELECT numero_cheque FROM cabecera_cheque";
-             using (SqlConnection con = new SqlConnection(ConexionUtil.CadenaClientes))
-             {
-                 con.Open();
-                 using (SqlCommand com = new SqlCommand(sql, con))
-                 {
-                     using (SqlDataReader resultado = com.ExecuteReader())
-                     {
-                         if (resultado.Read())
-                         {
-                             pedidoEncontrado = new Mensaje()
-                             {
-                                 dni = (string)resultado["numero_cheque"]
 
-                             };
-                             pedidosEncontrados.Add(pedidoEncontrado);
-                         }
-                     }
-                 }
-             }*/
             return null;//pedidosEncontrados;
         }
 
